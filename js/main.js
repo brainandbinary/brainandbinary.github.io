ddb4f7b59dcef189c32dadedc7dc381307c4e78d
mahadia hossain <mahadihossainpvt@gmail.com>
2020-11-06 23:32:46 +0600

    xxx

--- js/main.js
+++ js/main.js
@@ -1,26 +1,4 @@
-/*
-* SAPHIR - The Coming Soon Template
-* Build Date: May 2018
-* Last Update: May 2018
-* Author: Madeon08
-* Copyright (C) 2018-2XXX Madeon08
-* This is a premium product available exclusively here : https://themeforest.net/user/Madeon08/portfolio
-*/
 
-/*  TABLE OF CONTENTS
-    ---------------------------
-    1. Loading / Opening
-    2. FullPage init
-    3. Animate scroll fade adding
-    4. Menu button
-    5. Transition for nav & logo position
-    6. Text rotator on loading screen
-    7. Newsletter init
-*/
-
-/* ------------------------------------- */
-/* 1. Loading / Opening ................ */
-/* ------------------------------------- */
 
 $(window).on('load', function () {
     "use strict";
@@ -31,44 +9,44 @@ $(window).on('load', function () {
         
         $(".loader").addClass('loaded');
 
-    },2600);
+    },1);
 
     setTimeout(function(){
         
         $("#loading").addClass('loaded');
 
-    },2800);
+    },2);
 
     setTimeout(function(){
 
         $(".brand-logo").removeClass('before-loading').addClass('loaded');
 
-    },3000);
+    },3);
 
     setTimeout(function(){
 
         $("#fp-nav, .main-content").removeClass('before-loading').addClass('loaded');
 
-    },3200);
+    },3);
 
     setTimeout(function(){
 
         $("footer").removeClass('before-loading').addClass('loaded');
 
-    },3400);
+    },4);
 
     setTimeout(function(){
 
         $("#loading").remove();
         $(".brand-logo, footer").addClass('after-load');
 
-    },3600);
+    },5);
 
     setTimeout(function(){
 
         $("#fp-nav").addClass('notransition');
 
-    },4800);
+    },6);
 
 });
 
