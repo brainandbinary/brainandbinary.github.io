bd6e053ef17896d4c8ea9c904c5989919922e27a
Brain and Binary <48818899+brainandbinary@users.noreply.github.com>
2020-12-01 23:38:14 +0600

    Update cdn.js

--- js/cdn.js
+++ js/cdn.js
@@ -1,1 +1,1 @@
-alert("Hello world")
+$('#ad').html('<img class="img-fluid img-thumbnail " src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRU3sNdMzWjgHm4NpefuhtpdftNkg-kbvKcSKDRZGCGsGBoaPBv">');
