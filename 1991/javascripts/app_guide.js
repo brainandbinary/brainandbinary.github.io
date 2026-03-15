fa531be95d7e1b9ebb1482c58ddf2d2d01996ce9
Md Mahadia Hossain <mhbdpro@gmail.com>
2026-03-15 17:59:24 +0600

    app guide js added

---
+++ 1991/javascripts/app_guide.js
@@ -0,0 +1,12 @@
+window.app_guide = {
+
+  video: {
+    title: "How to locate your .ednet folder",
+
+    iframe: `
+      <div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><iframe src="https://fast.wistia.net/embed/iframe/ggx7yoydvt?web_component=true&seo=false" title="2026-03-15 17-37-08 Video" allow="autoplay; fullscreen" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" width="100%" height="100%"></iframe></div></div>
+
+    `
+  }
+
+};
